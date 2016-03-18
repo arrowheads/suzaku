4f539a9c1e2575ebaef0bac32bf9d7bacb4bcee0
Luiz de Milon <kori@openmailbox.org>
2016-03-18 00:54:38 -0400

    Add sinit config.

---
+++ sinit.config.h
@@ -0,0 +1,3 @@
+static char *const rcinitcmd[]     = { "/etc/suzaku/fly", NULL };
+static char *const rcrebootcmd[]   = { "/sbin/pavo", "reboot", NULL };
+static char *const rcpoweroffcmd[] = { "/sbin/pavo", "shutdown", NULL };
