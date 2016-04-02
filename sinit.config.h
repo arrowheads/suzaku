309d9791ce99f68e0579683be51d4cf4791f15fe
Luiz de Milon <kori@users.noreply.github.com>
2016-04-02 15:27:39 -0400

    Update sinit.config.h

--- sinit.config.h
+++ sinit.config.h
@@ -1,3 +1,3 @@
-static char *const rcinitcmd[]     = { "/etc/suzaku/fly", NULL };
+static char *const rcinitcmd[]     = { "/etc/suzaku/takeoff", NULL };
 static char *const rcrebootcmd[]   = { "/sbin/pavo", "reboot", NULL };
 static char *const rcpoweroffcmd[] = { "/sbin/pavo", "shutdown", NULL };
